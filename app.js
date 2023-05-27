e8207a41874ff27e575ea7ee041fe7cdc258664d
harshbix <kidabixson@gmail.com>
2023-05-27 15:13:22 +0300

    added condition that age can not be negative

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const inputYear = document.querySelector('#birthDate');
-const calculateBtn = document.querySelector('#calculate');
-const ageElement = document.querySelector('#age');
+const inputYear = document.querySelector("#birthDate");
+const calculateBtn = document.querySelector("#calculate");
+const ageElement = document.querySelector("#age");
 
-calculateBtn.addEventListener('click', () => {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    
-    const birthYear = parseInt(inputYear.value); // Retrieve and convert input value to a number
-    const age = currentYear - birthYear;
-    
-    ageElement.innerHTML = 'You are ' + age + ' years old.';
-    
-    // Save the age to local storage
-    localStorage.setItem('age', age);
+calculateBtn.addEventListener("click", () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  const birthYear = parseInt(inputYear.value);
+  const age = currentYear - birthYear;
+
+  if (age >= 0) {
+    ageElement.innerHTML = "You are " + age + " years old.";
+    localStorage.setItem("age", age);
+  } else {
+    alert('Invalid Birth year')
+    localStorage.removeItem("age");
+  }
 });
 
-// Retrieve the age from local storage on page load
-window.addEventListener('load', () => {
-    const savedAge = localStorage.getItem('age');
-    if (savedAge) {
-        ageElement.innerHTML = 'You are ' + savedAge + ' years old.';
-    }
+window.addEventListener("load", () => {
+  const savedAge = localStorage.getItem("age");
+  if (savedAge) {
+    ageElement.innerHTML = "You are " + savedAge + " years old.";
+  }
 });
